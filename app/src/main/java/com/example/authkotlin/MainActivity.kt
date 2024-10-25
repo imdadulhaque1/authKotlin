a34f656a1cbf93fa9af40d41a1842578e02f47f6
imdadulhaque1 <imdadulhaque1440@gmail.com>
2024-10-25 23:31:07 +0600

    Auth Kotin App

---
+++ app/src/main/java/com/example/authkotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.authkotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+       setContentView(R.layout.activity_main)
+    }
+}
+
