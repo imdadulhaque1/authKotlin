69ae6e8250454943063f67aa89b6584895c75f2b
imdadulhaque1 <imdadulhaque1440@gmail.com>
2024-10-26 08:14:35 +0600

    Implementing Binding in Auth MainActivity

--- app/src/main/java/com/example/authkotlin/MainActivity.kt
+++ app/src/main/java/com/example/authkotlin/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.authkotlin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-
+import com.example.authkotlin.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    // Declare Binding Variable
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Inflate the binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // Set the content view to binding's root
+        setContentView(binding.root)
         enableEdgeToEdge()
-       setContentView(R.layout.activity_main)
+
+        // Now, this line works correctly without setting the layout twice
+        binding.navigateToSignUp.setOnClickListener {
+            Toast.makeText(this, "Navigating to Sign-Up!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
-
